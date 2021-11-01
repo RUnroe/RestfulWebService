3c32d8ee907f7d36eb421f420c36f8dc24c11e46
RUnroe <ryanunroe@gmail.com>
2021-10-31 18:18:30 -0600

    Initial commit

---
+++ controller/src/test/java/edu/neumont/csc180/unroe/ryan/controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neumont.csc180.unroe.ryan.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
