b694bf9e3d2986b2346410f5f309657412e8cfdc
RUnroe <ryanunroe@gmail.com>
2021-11-01 17:01:43 -0600

    Allow image to rotate clockwise by 90 deg

--- service/src/main/java/edu/neumont/csc180/unroe/ryan/ImageService.java
+++ service/src/main/java/edu/neumont/csc180/unroe/ryan/ImageService.java
@@ -4,6 +4,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -76,10 +78,30 @@ public class ImageService {
     }
 
     public byte[] getRotatedImage(String name) {
-        byte[] image = getImage(name);
+        //https://blog.idrsolutions.com/2019/05/image-rotation-in-java/
+        BufferedImage image = getBufferedImage(name);
+        if(image == null) return null;
         //rotate image
+        final double rads = Math.toRadians(90);
+        final double sin = Math.abs(Math.sin(rads));
+        final double cos = Math.abs(Math.cos(rads));
+        final int w = (int) Math.floor(image.getWidth() * cos + image.getHeight() * sin);
+        final int h = (int) Math.floor(image.getHeight() * cos + image.getWidth() * sin);
+        final BufferedImage rotatedImage = new BufferedImage(w, h, image.getType());
+        final AffineTransform at = new AffineTransform();
+        at.translate(w / 2, h / 2);
+        at.rotate(rads,0, 0);
+        at.translate(-image.getWidth() / 2, -image.getHeight() / 2);
+        final AffineTransformOp rotateOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+        rotateOp.filter(image,rotatedImage);
 
-        return null;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(rotatedImage, "png", outputStream);
+        } catch (IOException e) {
+            return null;
+        }
+        return outputStream.toByteArray();
     }
 
     public boolean createImage(MultipartFile image) {
