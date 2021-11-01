625240dcdc2422586af56c3cbdbb6f0fb8266142
RUnroe <ryanunroe@gmail.com>
2021-10-31 19:39:38 -0600

    Create RestImage class

--- service/src/main/java/edu/neumont/csc180/unroe/ryan/ImageService.java
+++ service/src/main/java/edu/neumont/csc180/unroe/ryan/ImageService.java
@@ -1,4 +1,23 @@
 package edu.neumont.csc180.unroe.ryan;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class ImageService {
+    List<RestImage> images = new ArrayList<>();
+
+    public BufferedImage getImage(String name) {
+        BufferedImage selectedImage = null;
+        for (RestImage image: images) {
+            if(Objects.equals(image.name, name)) {
+                selectedImage = image.image;
+            }
+        }
+        return selectedImage;
+    }
+
+
 }
---
+++ service/src/main/java/edu/neumont/csc180/unroe/ryan/RestImage.java
@@ -0,0 +1,14 @@
+package edu.neumont.csc180.unroe.ryan;
+
+import java.awt.image.BufferedImage;
+
+public class RestImage {
+
+    public String name;
+    public BufferedImage image;
+
+    public RestImage(String name, BufferedImage image) {
+        this.name = name;
+        this.image = image;
+    }
+}
